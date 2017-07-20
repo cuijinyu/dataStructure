1035ccfc680ffe43fa16a4c5d5762b25b8c3ebb4
崔晋瑜 <2510271615@qq.com>
2017-07-20 19:32:30 +0800

    Add files via upload

---
+++ Java/list/list.java
@@ -0,0 +1,14 @@
+package chain;
+public class Node{
+	public Node next;
+	public int data;
+	public Node(int data){
+		this.data=data;
+	}
+	public void display(){
+		System.out.println(this.data);
+	}
+}
+public class List{
+	
+}
